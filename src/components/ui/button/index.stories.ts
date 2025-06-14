544f87d7ae0d340cb52020aa4861476eb0f9209f
もっくん <mokkunpc@gmail.com>
2025-06-14 14:00:58 +0900

    feat: add chadcn/ui

---
+++ src/components/ui/button/index.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import { Button } from ".";
+
+const meta: Meta<typeof Button> = {
+  title: "Button",
+  component: Button,
+  tags: ["autodocs"],
+  args: {
+    children: "ボタン",
+  },
+  argTypes: {
+    variant: {
+      control: "select",
+      options: [
+        "default",
+        "destructive",
+        "outline",
+        "secondary",
+        "ghost",
+        "link",
+      ],
+    },
+    size: {
+      control: "select",
+      options: ["default", "sm", "lg", "icon"],
+    },
+    asChild: {
+      control: "boolean",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    children: "ボタン",
+    variant: "default",
+    size: "default"
+  },
+};
