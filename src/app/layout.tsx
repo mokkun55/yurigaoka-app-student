e2cc39a4f63599ec228f803f252ceaf013fc9fa4
もっくん <mokkunpc@gmail.com>
2025-06-15 17:54:06 +0900

    feat: chadcn setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { M_PLUS_1p } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const mPlus1p = M_PLUS_1p({
+  weight: ["100", "300", "400", "500", "700", "800", "900"],
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-m-plus-1p",
 });
 
 export const metadata: Metadata = {
@@ -23,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ja">
+      <body className={`${mPlus1p.variable} antialiased`}>{children}</body>
     </html>
   );
 }
