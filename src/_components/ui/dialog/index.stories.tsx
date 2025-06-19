f5f56c2e6c4886b5dda8e963f82413e3d0a59ff8
もっくん <mokkunpc@gmail.com>
2025-06-19 13:41:35 +0900

    feat: dialog components

---
+++ src/_components/ui/dialog/index.stories.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/_components/ui/button";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/_components/ui/dialog";
+
+import type { Meta, StoryObj } from "@storybook/nextjs";
+
+const DialogMock = () => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button>Open</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>ダイアログタイトル</DialogTitle>
+          <DialogDescription>
+            ダイアログの説明文 Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Quod eveniet, magnam cumque saepe unde, veritatis,
+            qui reprehenderit quaerat atque laborum repellat? Laboriosam
+            necessitatibus suscipit odit molestiae minus amet cum.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="flex flex-col gap-2">
+          <Button>ボタン1</Button>
+          <Button>ボタン2</Button>
+
+          <DialogClose asChild>
+            <Button>閉じる</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const meta = {
+  title: "Dialog",
+  component: DialogMock,
+  tags: ["autodocs"],
+} satisfies Meta<typeof DialogMock>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    // propsをここに追加
+  },
+};
