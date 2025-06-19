0a6a4947062e15610628684f99515f71e1d18c19
もっくん <mokkunpc@gmail.com>
2025-06-19 12:32:10 +0900

    feat: check box filedのスタイル修正

--- src/_components/ui/checkbox/checkbox-field/index.tsx
+++ src/_components/ui/checkbox/checkbox-field/index.tsx
@@ -9,7 +9,7 @@ export const CheckboxField = ({ label }: Props) => {
   return (
     <div className="flex items-center gap-2 border border-(--border-gray) rounded-md p-2">
       <Label className="w-full">
-        <Checkbox className="border-black w-5 h-5" />
+        <Checkbox className="border-black border-2 rounded-none w-5 h-5" />
         <span>{label}</span>
       </Label>
     </div>
