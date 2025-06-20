0ab59a92f9377734432e796a947ef4f7f33e4ca4
もっくん <mokkunpc@gmail.com>
2025-06-20 12:28:47 +0900

    feat: loading spinner

---
+++ src/_components/ui/loading-spinner/index.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+
+type Props = {
+  size?: number;
+  className?: string;
+};
+
+export default function LoadingSpinner({ className, size = 24 }: Props) {
+  return (
+    <div className="flex items-center justify-center">
+      <Loader2 className={cn("animate-spin", className)} size={size} />
+    </div>  
+  );
+}
