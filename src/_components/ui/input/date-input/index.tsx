5cbd8d5091e7b780f13de79d5278a198b8f0a718
もっくん <mokkunpc@gmail.com>
2025-07-03 15:31:14 +0900

    time inputを使う

--- src/_components/ui/input/date-input/index.tsx
+++ src/_components/ui/input/date-input/index.tsx
@@ -4,12 +4,12 @@ import { cn } from '@/lib/utils'
 // TODO placeholder問題どうにかする
 export function DateInput({ className, ...props }: React.ComponentProps<'input'>) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full">
       <BaseInput
         icon="calendar"
         type="date"
         id="date-picker"
-        defaultValue={new Date().toISOString().split('T')[0]}
+        fullWidth
         className={cn(
           `bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none`,
           className
