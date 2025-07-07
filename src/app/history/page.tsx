c299f255ffd96cd6e22a89a0489f288e807f718c
もっくん <mokkunpc@gmail.com>
2025-07-07 22:43:53 +0900

    モックでの表示

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from 'next/navigation'
 import Header from '@/_components/ui/header'
 import Footer from '@/_components/ui/footer'
+import HistoryCard from './_components/history-card'
 
 export default function HistoryPage() {
   const router = useRouter()
@@ -10,7 +11,31 @@ export default function HistoryPage() {
   return (
     <div>
       <Header title="申請履歴" type="back" onClick={() => router.back()} />
-      <div className="p-3">履歴</div>
+      <div className="p-3">
+        <p className="text-center text-sm">申請履歴をクリックで詳細を見ることができます</p>
+        <div className="mt-3 flex flex-col gap-4 overflow-y-auto max-h-[calc(100vh-200px)]">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <HistoryCard
+              key={index}
+              status={index % 2 === 0 ? 'pending' : 'approved'}
+              type={index % 2 === 0 ? 'homecoming' : 'meal'}
+              createdAt="2025/01/01"
+              homecoming={{
+                id: '1',
+                place: '実家',
+              }}
+              period={{
+                startDate: new Date('2025/01/01 12:00'),
+                endDate: new Date('2025/01/01 14:00'),
+              }}
+              meal={{
+                startDate: { morning: false, evening: true },
+                endDate: { morning: false, evening: true },
+              }}
+            />
+          ))}
+        </div>
+      </div>
       <Footer activeTab="history" />
     </div>
   )
